08974c33f0a082df407ecbeed0c62199823ebe19
Prashant <prashantdubey0098@gmail.com>
2024-01-11 13:54:06 +0530

    added media queries on home

--- app/page.jsx
+++ app/page.jsx
@@ -11,7 +11,12 @@ export default function Home() {
     <main className="main__container relative flex h-[100vh] w-full items-center justify-evenly">
       <div className="header__container relative flex flex-col gap-2">
         <div className="backdrop__container fixed left-[50%] top-1 z-[-1] -translate-x-[50%] transform opacity-5">
-          <Image src={logoIcon} alt="background logo" height={700} />
+          <Image
+            src={logoIcon}
+            alt="background logo"
+            height={700}
+            objectFit="contain"
+          />
         </div>
         <div className="heading__container">
           <h1
